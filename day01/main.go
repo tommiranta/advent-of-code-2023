d76b9167b68098d512dc29339d17c2fbc5e90a5d
tommiranta <tommi.ranta@leadfeeder.com>
2023-12-02 16:49:23 +0200

    day 1 part 2

--- day01/main.go
+++ day01/main.go
@@ -2,38 +2,120 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
-func parseLine(text string) (int, error) {
-	const zero = 48
-	const nine = zero + 9
-	var a, b int
+const UNICODE_ZERO = 48
+const UNICODE_NINE = UNICODE_ZERO + 9
 
-	for _, ch := range text {
-		if ch >= zero && ch <= nine {
-			if a == 0 {
-				a = int(ch)
-				b = int(ch)
-			} else {
-				b = int(ch)
-			}
+var NUMBERS_AS_LETTERS = [10]string{
+	"zero",
+	"one",
+	"two",
+	"three",
+	"four",
+	"five",
+	"six",
+	"seven",
+	"eight",
+	"nine",
+}
+
+func findFirstDigit(text string) (int, int) {
+	for idx, ch := range text {
+		if ch >= UNICODE_ZERO && ch <= UNICODE_NINE {
+			return int(ch - UNICODE_ZERO), idx
 		}
 	}
+	return -1, -1
+}
 
-	if a > 0 && b > 0 {
-		return (a-zero)*10 + (b - zero), nil
+func findLastDigit(text string) (int, int) {
+	for i := len(text) - 1; i >= 0; i-- {
+		ch := text[i]
+		if ch >= UNICODE_ZERO && ch <= UNICODE_NINE {
+			return int(ch - UNICODE_ZERO), i
+		}
+	}
+	return -1, -1
+}
+
+func findFirstDigitAsLetters(text string) (int, int) {
+	digit_winning := -1
+	idx_winning := -1
+
+	for value, letters := range NUMBERS_AS_LETTERS {
+		idx := strings.Index(text, letters)
+		if idx != -1 && (idx_winning == -1 || idx < idx_winning) {
+			digit_winning = value
+			idx_winning = idx
+		}
+	}
+	return digit_winning, idx_winning
+}
+
+func findLastDigitAsLetters(text string) (int, int) {
+	digit_winning := -1
+	idx_winning := -1
+
+	for value, letters := range NUMBERS_AS_LETTERS {
+		idx := strings.LastIndex(text, letters)
+		if idx != -1 && (idx_winning == -1 || idx > idx_winning) {
+			digit_winning = value
+			idx_winning = idx
+		}
+	}
+	return digit_winning, idx_winning
+}
+
+func digitsToNumber(double_digit int, single_digit int) int {
+	return double_digit*10 + single_digit
+}
+
+func parseLineNoLetters(text string) int {
+	double_digit, _ := findFirstDigit(text)
+	single_digit, _ := findLastDigit(text)
+	if single_digit != -1 && double_digit != -1 {
+		return digitsToNumber(double_digit, single_digit)
+	}
+	return -1
+}
+
+func parseLine(text string) int {
+	single_digit_winner := -1
+	double_digit_winner := -1
+
+	double_digit, idx_double_digit := findFirstDigit(text)
+	single_digit, idx_single_digit := findLastDigit(text)
+
+	double_digit_letters, idx_double_digit_letters := findFirstDigitAsLetters(text)
+	single_digit_letters, idx_single_digit_letters := findLastDigitAsLetters(text)
+
+	if idx_double_digit_letters != -1 && idx_double_digit_letters < idx_double_digit {
+		double_digit_winner = double_digit_letters
 	} else {
-		return -1, errors.New("Did not find digits in string.")
+		double_digit_winner = double_digit
 	}
 
+	if idx_single_digit_letters != -1 && idx_single_digit_letters > idx_single_digit {
+		single_digit_winner = single_digit_letters
+	} else {
+		single_digit_winner = single_digit
+	}
+
+	if single_digit_winner != -1 && double_digit_winner != -1 {
+		return digitsToNumber(double_digit_winner, single_digit_winner)
+	}
+	return -1
 }
 
 func main() {
-	sum := 0
+	sum_pt1 := 0
+	sum_pt2 := 0
+
 	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -42,17 +124,25 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		number, err := parseLine(scanner.Text())
-		if err != nil {
+		number := parseLineNoLetters(scanner.Text())
+		if number == -1 {
+			log.Fatal("Parsing digits failed")
 			break
 		}
-		sum += number
+		sum_pt1 += number
+
+		number = parseLine(scanner.Text())
+		if number == -1 {
+			log.Fatal("Parsing digits failed")
+			break
+		}
+		sum_pt2 += number
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(sum)
-
+	fmt.Println("Solution part 1:", sum_pt1)
+	fmt.Println("Solution part 2:", sum_pt2)
 }
